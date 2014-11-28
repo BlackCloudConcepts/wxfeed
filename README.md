385daa4d010d681c1b7ed3f23e9aeda7dbd09035
blackcloudconcepts <blackcloudconcepts@gmail.com>
2014-11-28 11:58:51 -0500

    initial project iteration

---
+++ README.md
@@ -0,0 +1,1 @@
+Node application which feeds weather data from http://openweathermap.org into Firebase to be read from /wxapp.
---
+++ firebase-server.js
@@ -0,0 +1,40 @@
+var request = require('request');
+var Firebase = require('firebase');
+var dataRef = new Firebase('https://resplendent-heat-1209.firebaseio.com/wx/');
+
+var locations = [
+	'Austin,TX',
+	'Buda,TX',
+	'Burnet,TX',
+	'Dripping Springs,TX',
+	'Fredericksburg,TX',
+	'Georgetown,TX',
+	'Johnson City,TX',
+	'Llano,TX',
+	'Lockhart,TX',
+	'New Braunfels,TX',
+	'San Antonio,TX',
+	'San Marcos,TX',
+	'Temple,TX'
+];
+setInterval(function(){
+        for (var i = 0;i < locations.length;i++){
+                getWeatherData(locations[i]);
+        }
+}, 10000);
+
+function getWeatherData(loc){
+        var options = {
+                url: 'http://api.openweathermap.org/data/2.5/weather?q='+loc+'&APPID=4ad88837f1ea247954d4011aac2e4c9c',
+                method: 'GET'
+        }
+
+        request(options, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                        var data = JSON.parse(body);
+                        var obj = {};
+                        obj['_'+loc] = data;
+                        dataRef.update(obj);
+                }
+        })
+}

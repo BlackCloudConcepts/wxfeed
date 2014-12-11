d19b66791bafa4569c4da7770921df6530a3eecc
blackcloudconcepts <blackcloudconcepts@gmail.com>
2014-12-11 12:29:10 -0500

    add region

--- firebase-server.js
+++ firebase-server.js
@@ -3,27 +3,27 @@ var Firebase = require('firebase');
 var dataRef = new Firebase('https://resplendent-heat-1209.firebaseio.com/wx/');
 
 var locations = [
-	'Austin,TX',
-	'Buda,TX',
-	'Burnet,TX',
-	'Dripping Springs,TX',
-	'Fredericksburg,TX',
-	'Georgetown,TX',
-	'Johnson City,TX',
-	'Llano,TX',
-	'Lockhart,TX',
-	'New Braunfels,TX',
-	'San Antonio,TX',
-	'San Marcos,TX',
-	'Temple,TX',
-	'Chickasha,OK',
-	'El Reno,OK',
-	'Guthrie,OK',
-	'Harrah,OK',
-	'Kingfisher,OK',
-	'Moore,OK',
-	'Norman,OK',
-	'Oklahoma City,OK',
+	{city:'Austin,TX',region:'TX'},
+	{city:'Buda,TX',region:'TX'},
+	{city:'Burnet,TX',region:'TX'},
+	{city:'Dripping Springs,TX',region:'TX'},
+	{city:'Fredericksburg,TX',region:'TX'},
+	{city:'Georgetown,TX',region:'TX'},
+	{city:'Johnson City,TX',region:'TX'},
+	{city:'Llano,TX',region:'TX'},
+	{city:'Lockhart,TX',region:'TX'},
+	{city:'New Braunfels,TX',region:'TX'},
+	{city:'San Antonio,TX',region:'TX'},
+	{city:'San Marcos,TX',region:'TX'},
+	{city:'Temple,TX',region:'TX'},
+	{city:'Chickasha,OK',region:'OK'},
+	{city:'El Reno,OK',region:'OK'},
+	{city:'Guthrie,OK',region:'OK'},
+	{city:'Harrah,OK',region:'OK'},
+	{city:'Kingfisher,OK',region:'OK'},
+	{city:'Moore,OK',region:'OK'},
+	{city:'Norman,OK',region:'OK'},
+	{city:'Oklahoma City,OK',region:'OK'}
 ];
 setInterval(function(){
         for (var i = 0;i < locations.length;i++){
@@ -33,15 +33,16 @@ setInterval(function(){
 
 function getWeatherData(loc){
         var options = {
-                url: 'http://api.openweathermap.org/data/2.5/weather?q='+loc+'&APPID=4ad88837f1ea247954d4011aac2e4c9c',
+                url: 'http://api.openweathermap.org/data/2.5/weather?q='+loc.city+'&APPID=4ad88837f1ea247954d4011aac2e4c9c',
                 method: 'GET'
         }
 
         request(options, function (error, response, body) {
                 if (!error && response.statusCode == 200) {
                         var data = JSON.parse(body);
+			data.region = loc.region;
                         var obj = {};
-                        obj['_'+loc] = data;
+                        obj['_'+loc.city] = data;
                         dataRef.update(obj);
                 }
         })
